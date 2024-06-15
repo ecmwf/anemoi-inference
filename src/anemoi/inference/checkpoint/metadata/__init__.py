9b46a6f0bae77343054136ba48fc6e9490676043
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-06-15 21:37:55 +0100

    work on graph

--- src/anemoi/inference/checkpoint/metadata/__init__.py
+++ src/anemoi/inference/checkpoint/metadata/__init__.py
@@ -351,12 +351,12 @@ class Metadata:
         for node, label in nodes.items():
             for k, v in label.items():
 
-                if isinstance(v, str) and v.startswith("<") and v.endswith(">"):
-                    # Skip HTML labels
-                    pass
-                else:
-                    # Use json.dumps to escape special characters
-                    label[k] = json.dumps(v)
+                # if isinstance(v, str) and v.startswith("<") and v.endswith(">"):
+                #     # Keep HTML labels as is
+                #     pass
+                # else:
+                #     # Use json.dumps to escape special characters
+                label[k] = json.dumps(v)
 
             label = " ".join([f"{k}={v}" for k, v in label.items()])
 
