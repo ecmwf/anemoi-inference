a8b0c45834d1d89836120ef9f55606b45ebb4fcd
Harrison Cook <harrison.cook@ecmwf.int>
2024-09-23 09:58:12 +0000

    Remove self

--- src/anemoi/inference/checkpoint/utils.py
+++ src/anemoi/inference/checkpoint/utils.py
@@ -1,14 +1,12 @@
-
-
+from __future__ import annotations
 
 from dataclasses import dataclass
 from functools import total_ordering
-from typing import Self
 
 
 @dataclass
 @total_ordering
-class Version():
+class Version:
     version_str: str
 
     def _split(self):
@@ -17,9 +15,11 @@ class Version():
     @property
     def major(self) -> int:
         return int(self._split()[0])
+
     @property
     def minor(self) -> int:
         return int(self._split()[1])
+
     @property
     def patch(self) -> int | str:
         patchval = self._split()[2]
@@ -30,37 +30,37 @@ class Version():
 
     def __str__(self):
         return f"{self.major}.{self.minor}.{self.patch}"
-    
+
     def __repr__(self) -> str:
         return f"Version({self.version_str})"
-    
-    def __eq__(self, value: Self) -> bool:
+
+    def __eq__(self, value: Version) -> bool:
         return self._split() == value._split()
-    
-    def __lt__(self, value: Self) -> bool:
+
+    def __lt__(self, value: Version) -> bool:
         if not isinstance(value, Version):
             return NotImplemented
-        
+
         for self_part, value_part in zip([self.major, self.minor], [value.major, value.minor]):
             if self_part > value_part:
                 return False
             elif self_part < value_part:
                 return True
-            
+
         if not isinstance(self.patch, str) and not isinstance(value.patch, str):
             return self.patch < value.patch
         return False
 
-    def __gt__(self, value: Self) -> bool:
+    def __gt__(self, value: Version) -> bool:
         if not isinstance(value, Version):
             return NotImplemented
-        
+
         for self_part, value_part in zip([self.major, self.minor], [value.major, value.minor]):
             if self_part > value_part:
                 return True
             elif self_part < value_part:
                 return False
-            
+
         if not isinstance(self.patch, str) and not isinstance(value.patch, str):
             return self.patch > value.patch
         return False
