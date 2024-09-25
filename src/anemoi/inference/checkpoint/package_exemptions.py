13ea14f02665677eed836ad4146dbe18c4ab401f
Harrison Cook <harcook129@gmail.com>
2024-09-25 13:07:36 +0100

    Feature/Add method to validate environment in a checkpoint (#13)
    * Feature: Add environment checking
    - Add property for `provenance_training` in Metadata
    - Check environment on `validate_environment`
    
    * chore: Update changelog
    
    * Fix: Git record difference check
    
    * Fix: Cull categories with no messages
    
    * Add PR Tag
    
    * Make link
    
    * Add exempt packages from the checking
    
    * Change return value
    
    * Add Version class
    
    * Address comments
    - Improve error messaging
    - Get inference environment from pkg
    - Allow patch comparison
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove self
    
    * Replace custom version with packaging
    
    * Fix exemption
    
    * Replace relative imports
    
    * Add more exempt packages
    - Protect packake_exemptions.py in CODEOWNERS
    
    * Address naming issues
    - Attempt an import to fix basic python modules being missed
    - Add ignore
    
    * Add __future__ annotations
    
    * Add --validate to inspect subcommand
    
    * Changelog
    
    ---------
    
    Co-authored-by: Gert Mertes <gert.mertes@ecmwf.int>

---
+++ src/anemoi/inference/checkpoint/package_exemptions.py
@@ -0,0 +1,15 @@
+# Complete package name to be exempt
+EXEMPT_PACKAGES = [
+    "anemoi.training",
+    "hydra",
+    "hydra_plugins",
+    "lightning",
+    "pytorch_lightning",
+    "lightning_fabric",
+    "lightning_utilities",
+]
+
+# Entire namespaces to be exempt
+EXEMPT_NAMESPACES = [
+    "hydra_plugins",
+]
