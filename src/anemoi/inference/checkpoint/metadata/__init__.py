b0edd47c365d49376a5b43d2febc56f569ccd59e
Marek Jacob <1129-b380572@users.noreply.gitlab.dkrz.de>
2024-10-14 12:12:51 +0200

    undo the changes to the copyright statements

--- src/anemoi/inference/checkpoint/metadata/__init__.py
+++ src/anemoi/inference/checkpoint/metadata/__init__.py
@@ -1,10 +1,10 @@
 # (C) Copyright 2023 European Centre for Medium-Range Weather Forecasts.
-# (C) Copyright 2024 Deutscher Wetterdienst.
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# In applying this licence, the above institution do not waive the privileges
-# and immunities granted to it by virtue of its status as an intergovernmental
-# organisation  nor does it submit to any jurisdiction.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 
 import json
 import logging
