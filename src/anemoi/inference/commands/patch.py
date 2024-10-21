f2a8ca941650bac4a0bbfe64f1921d9dfab79e09
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-10-21 10:39:17 +0000

    add patch command

---
+++ src/anemoi/inference/commands/patch.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# (C) Copyright 2024 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+
+import logging
+from copy import deepcopy
+
+from . import Command
+
+LOG = logging.getLogger(__name__)
+
+
+class PatchCmd(Command):
+    """Patch a checkpoint file."""
+
+    need_logging = True
+    _cache = {}
+
+    def add_arguments(self, command_parser):
+        command_parser.add_argument("path", help="Path to the checkpoint.")
+
+    def run(self, args):
+        from anemoi.utils.checkpoints import load_metadata
+        from anemoi.utils.checkpoints import replace_metadata
+
+        from anemoi.inference.checkpoint.metadata import Metadata
+
+        original_metadata = load_metadata(args.path)
+        metadata = deepcopy(original_metadata)
+
+        # Patch the metadata
+        while True:
+            previous = deepcopy(metadata)
+            metadata = Metadata.from_metadata(metadata).patch_metadata(metadata, self)
+            if metadata == previous:
+                break
+            LOG.info("Metadata patched")
+
+        if metadata != original_metadata:
+            LOG.info("Patching metadata")
+            replace_metadata(args.path, metadata)
+
+    def _find(self, where, what, matches=None):
+        if matches is None:
+            matches = []
+
+        if isinstance(where, dict):
+            for key, value in where.items():
+                if value == what:
+                    matches.append(value)
+                else:
+                    self._find(value, what, matches)
+
+        elif isinstance(where, list):
+            for item in where:
+                self._find(item, what, matches)
+
+        return matches
+
+    def patch_zarr(self, zarr_attributes, metadata):
+
+        matches = self._find(metadata, zarr_attributes)
+        assert len(matches) > 0
+
+        # If we have multiple matches, we will
+        # handle them in the next iteration
+        zarr_attributes = matches[0]
+
+        uuid = zarr_attributes["uuid"]
+
+        for key in ("constants", "variables_metadata"):
+
+            entry = self._fetch(uuid)
+            if key in entry["metadata"]:
+                zarr_attributes[key] = entry["metadata"][key]
+            else:
+                LOG.warning(f"No '{key}' found for dataset '{uuid}'")
+
+        return metadata
+
+    def _fetch(self, uuid):
+        from anemoi.registry import Dataset
+        from anemoi.registry.entry.dataset import DatasetCatalogueEntryList
+
+        if uuid in self._cache:
+            return self._cache[uuid]
+
+        LOG.info(f"Fetching metadata for dataset uuid {uuid}")
+
+        match = None
+        for e in DatasetCatalogueEntryList().get(params={"uuid": uuid}):
+            if match:
+                raise ValueError(f"Multiple entries found for uuid {uuid}")
+            match = e
+
+        if match is None:
+            raise ValueError(f"No entry found for uuid {uuid}")
+
+        name = match["name"]
+        LOG.info(f"Dataset is '{name}'")
+        LOG.info(f"https://anemoi.ecmwf.int/datasets/{name}")
+        entry = Dataset(name)
+
+        self._cache[uuid] = entry.record
+        return self._cache[uuid]
+
+    def _uuid_to_name(self, uuid):
+        entry = self._fetch(uuid)
+        return entry["name"]
+
+
+command = PatchCmd
---
+++ src/anemoi/inference/commands/requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# (C) Copyright 2024 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
+
+import logging
+
+from ..checkpoint import Checkpoint
+from . import Command
+
+LOG = logging.getLogger(__name__)
+
+
+class RequestsCmd(Command):
+    """Patch a checkpoint file."""
+
+    need_logging = False
+    _cache = {}
+
+    def add_arguments(self, command_parser):
+        command_parser.add_argument("path", help="Path to the checkpoint.")
+
+    def run(self, args):
+        c = Checkpoint(args.path)
+
+        for r in c.requests():
+            print(r)
+
+
+command = RequestsCmd
