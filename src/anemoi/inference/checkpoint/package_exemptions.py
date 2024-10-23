8ecf88fa4a245e59db1ba2712a500ceedd5c3c8f
b8raoult <53792887+b8raoult@users.noreply.github.com>
2024-10-23 12:13:21 +0100

    Update copyright notice

--- src/anemoi/inference/checkpoint/package_exemptions.py
+++ src/anemoi/inference/checkpoint/package_exemptions.py
@@ -1,4 +1,12 @@
-# Complete package name to be exempt
+# (C) Copyright 2024 Anemoi contributors.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 EXEMPT_PACKAGES = [
     "anemoi.training",
     "hydra",
