e6c25d72f1f5763630c5dc14d369809f710b9d46
Dieter Van den Bleeken <dieter.vandenbleeken@meteo.be>
2024-11-28 09:47:31 +0100

    add initial state writing to netcdf output

--- src/anemoi/inference/outputs/netcdf.py
+++ src/anemoi/inference/outputs/netcdf.py
@@ -53,9 +53,15 @@ class NetCDFOutput(Output):
 
         values = len(state["latitudes"])
 
+        time = 0
+        self.reference_date=state["date"]
+        if hasattr(self.context, "time_step") and hasattr(self.context, "lead_time"):
+            time = self.context.lead_time // self.context.time_step
+        if hasattr(self.context, "reference_date"):
+            self.reference_date = self.context.reference_date
+            
         self.values_dim = self.ncfile.createDimension("values", values)
-
-        self.time_dim = self.ncfile.createDimension("time", self.context.lead_time // self.context.time_step)
+        self.time_dim = self.ncfile.createDimension("time", time)
         self.time_var = self.ncfile.createVariable("time", "i4", ("time",), **compression)
 
         self.time_var.units = "seconds since {0}".format(self.context.reference_date)
@@ -99,12 +105,13 @@ class NetCDFOutput(Output):
         return self.ncfile
 
     def write_initial_state(self, state):
-        LOG.warning("NetCDFOutput: Writing of initial state is not supported.")
-
+        reduced_state = self.reduce(state)
+        self.write_state(reduced_state)
+        
     def write_state(self, state):
         self._init(state)
 
-        step = state["date"] - self.context.reference_date
+        step = state["date"] - self.reference_date
         self.time_var[self.n] = step.total_seconds()
 
         for name, value in state["fields"].items():
