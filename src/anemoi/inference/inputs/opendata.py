f1f4f930b34202796b993baee36943e7c02999bf
Harrison Cook <harrison.cook@ecmwf.int>
2025-03-17 10:36:09 +0000

    Update docstrings

--- src/anemoi/inference/inputs/opendata.py
+++ src/anemoi/inference/inputs/opendata.py
@@ -9,11 +9,22 @@
 
 
 import logging
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
 
 import earthkit.data as ekd
 from anemoi.utils.grib import shortname_to_paramid
 from earthkit.data.utils.dates import to_datetime
 
+from anemoi.inference.context import Context
+from anemoi.inference.types import DataRequest
+from anemoi.inference.types import Date
+from anemoi.inference.types import State
+
+from ..grib.templates import TemplateProvider
 from ..grib.templates import create_template_provider
 from . import input_registry
 from .grib import GribInput
@@ -23,8 +34,19 @@ LOG = logging.getLogger(__name__)
 SOIL_MAPPING = {"stl1": "sot", "stl2": "sot", "stl3": "sot", "swvl1": "vsw", "swvl2": "vsw", "swvl3": "vsw"}
 
 
-def postprocess_geopotential_height(fields: ekd.FieldList):
-    """Change geopotential height to meters and rename to `z`."""
+def postprocess_geopotential_height(fields: ekd.FieldList) -> ekd.FieldList:
+    """Change geopotential height to meters and rename to `z`.
+
+    Parameters
+    ----------
+    fields : ekd.FieldList
+        Fields to be post-processed.
+
+    Returns
+    -------
+    ekd.FieldList
+        Post-processed fields.
+    """
     r = ekd.FieldList()
     for field in fields:
         if field.metadata()["param"] == "gh":
@@ -34,8 +56,23 @@ def postprocess_geopotential_height(fields: ekd.FieldList):
     return r
 
 
-def _retrieve_soil(request: dict, soil_params: list[str]):
-    """Retrieve soil params"""
+def _retrieve_soil(request: dict, soil_params: list[str]) -> ekd.FieldList:
+    """Retrieve soil data.
+
+    Map the soil parameters to the correct ECMWF parameter IDs and levels.
+
+    Parameters
+    ----------
+    request : dict
+        Request for the soil data.
+    soil_params : list[str]
+        Parameters to be retrieved.
+
+    Returns
+    -------
+    ekd.FieldList
+        Soil data.
+    """
     levels = list(set(int(s[-1]) for s in soil_params))
     request["param"] = list(SOIL_MAPPING[s] for s in soil_params)
     request["levelist"] = levels
@@ -51,12 +88,33 @@ def _retrieve_soil(request: dict, soil_params: list[str]):
     return soil_data
 
 
-def regridding(fields: ekd.FieldList, grid: str, template):
-    """Apply regridding to the field."""
+def regridding(
+    fields: ekd.FieldList, grid: Optional[Union[str, List[float]]], area: Optional[List[float]], template: ekd.Field
+) -> ekd.FieldList:
+    """Apply regridding to the field.
+
+    Parameters
+    ----------
+    fields : ekd.FieldList
+        Fields to be regridded.
+    grid : Optional[Union[str, List[float]]]
+        Grid for the regridding.
+    area : Optional[List[float]]
+        Area for the regridding.
+    template : ekd.Field
+        Template for the regridding.
+
+    Returns
+    -------
+    ekd.FieldList
+        Regridded fields.
+    """
     import earthkit.regrid as ekr
     import numpy as np
 
     r = ekd.FieldList()
+
+    _ = area
 
     f_md = template.metadata()
 
@@ -78,9 +136,35 @@ def regridding(fields: ekd.FieldList, grid: str, template):
     return r
 
 
-def retrieve(requests, grid, area, template_provider, **kwargs):
+def retrieve(
+    requests: List[Dict[str, Any]],
+    grid: Optional[Union[str, List[float]]],
+    area: Optional[List[float]],
+    template_provider: TemplateProvider,
+    **kwargs: Any,
+) -> ekd.FieldList:
+    """Retrieve data from ECMWF Opendata.
 
-    def _(r):
+    Parameters
+    ----------
+    requests : List[Dict[str, Any]]
+        The list of requests to be retrieved.
+    grid : Optional[Union[str, List[float]]]
+        The grid for the retrieval.
+    area : Optional[List[float]]
+        The area for the retrieval.
+    template_provider : TemplateProvider
+        Template provider for the regridding.
+    **kwargs : Any
+        Additional keyword arguments.
+
+    Returns
+    -------
+    ekd.FieldList
+        The retrieved data.
+    """
+
+    def _(r: DataRequest):
         mars = r.copy()
         for k, v in r.items():
             if isinstance(v, (list, tuple)):
@@ -105,7 +189,7 @@ def retrieve(requests, grid, area, template_provider, **kwargs):
         if any(k in r["param"] for k in SOIL_MAPPING.keys()):
             requested_soil_variables = [k for k in SOIL_MAPPING.keys() if k in r["param"]]
             r["param"] = [p for p in r["param"] if p not in requested_soil_variables]
-            result += regridding(_retrieve_soil(r.copy(), requested_soil_variables), grid, template)
+            result += regridding(_retrieve_soil(r.copy(), requested_soil_variables), grid, area, template)
 
         LOG.debug("%s", _(r))
 
@@ -113,7 +197,7 @@ def retrieve(requests, grid, area, template_provider, **kwargs):
         for func in post_processing_functions:
             returned_data = func(returned_data)
 
-        result += regridding(returned_data, grid, template)
+        result += regridding(returned_data, grid, area, template)
 
     return result
 
@@ -122,7 +206,22 @@ def retrieve(requests, grid, area, template_provider, **kwargs):
 class OpenDataInput(GribInput):
     """Get input fields from ECMWF open-data"""
 
-    def __init__(self, context, *, namer=None, templates=None, **kwargs):
+    trace_name = "opendata"
+
+    def __init__(
+        self, context: Context, *, namer: Optional[Any] = None, templates: Optional[Union[str, dict]] = None, **kwargs
+    ):
+        """Initialize the OpenDataInput.
+
+        Parameters
+        ----------
+        context : Any
+            The context in which the input is used.
+        namer : Optional[Any]
+            Optional namer for the input.
+        templates : Optional[Union[str, dict]], optional
+            Optional specification for template provider, by default None
+        """
         super().__init__(context, namer=namer)
 
         self.variables = self.checkpoint.variables_from_input(include_forcings=False)
@@ -130,7 +229,19 @@ class OpenDataInput(GribInput):
 
         self.template_provider = create_template_provider(self, templates or "builtin")
 
-    def create_input_state(self, *, date):
+    def create_input_state(self, *, date: Optional[Date]) -> State:
+        """Create the input state for the given date.
+
+        Parameters
+        ----------
+        date : Optional[Date]
+            The date for which to create the input state.
+
+        Returns
+        -------
+        State
+            The created input state.
+        """
         if date is None:
             date = to_datetime(-1)
             LOG.warning("OpenDataInput: `date` parameter not provided, using yesterday's date: %s", date)
@@ -146,7 +257,21 @@ class OpenDataInput(GribInput):
             date=date,
         )
 
-    def retrieve(self, variables, dates):
+    def retrieve(self, variables: List[str], dates: List[Date]) -> Any:
+        """Retrieve data for the given variables and dates.
+
+        Parameters
+        ----------
+        variables : List[str]
+            The list of variables to retrieve.
+        dates : List[Any]
+            The list of dates for which to retrieve the data.
+
+        Returns
+        -------
+        Any
+            The retrieved data.
+        """
 
         requests = self.checkpoint.mars_requests(
             variables=variables,
@@ -163,8 +288,23 @@ class OpenDataInput(GribInput):
             requests, self.checkpoint.grid, self.checkpoint.area, template_provider=self.template_provider, **kwargs
         )
 
-    def template(self, variable, date, **kwargs):
-        return self.retrieve([variable], [date])[0]
+    def load_forcings_state(self, *, variables: List[str], dates: List[Date], current_state: State) -> State:
+        """Load the forcings state for the given variables and dates.
 
-    def load_forcings(self, variables, dates):
-        return self._load_forcings(self.retrieve(variables, dates), variables, dates)
+        Parameters
+        ----------
+        variables : List[str]
+            The list of variables for which to load the forcings state.
+        dates : List[Date]
+            The list of dates for which to load the forcings state.
+        current_state : State
+            The current state to be updated with the loaded forcings state.
+
+        Returns
+        -------
+        Any
+            The loaded forcings state.
+        """
+        return self._load_forcings_state(
+            self.retrieve(variables, dates), variables=variables, dates=dates, current_state=current_state
+        )
