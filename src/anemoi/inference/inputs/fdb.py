b9eca3c6126f4b9cfeba7782bcbbc30677b77a95
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-04-03 18:13:37 +0200

    param_id_map must be an empty dict by default

--- src/anemoi/inference/inputs/fdb.py
+++ src/anemoi/inference/inputs/fdb.py
@@ -60,7 +60,7 @@ class FDBInput(GribInput):
         self.fdb_config = fdb_config
         self.fdb_userconfig = fdb_userconfig
         # NOTE: this is a temporary workaround for #191 thus not documented
-        self.param_id_map = kwargs.pop("param_id_map", None)
+        self.param_id_map = kwargs.pop("param_id_map", {})
         self.variables = self.checkpoint.variables_from_input(include_forcings=False)
 
     def create_input_state(self, *, date: Optional[Date]) -> State:
