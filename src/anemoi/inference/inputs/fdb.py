5181700949fcfdd4dd0d25fb1d4ebf8315ae769c
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-04-03 18:12:18 +0200

    pass additional request keys are **kwargs (remove template dict)

--- src/anemoi/inference/inputs/fdb.py
+++ src/anemoi/inference/inputs/fdb.py
@@ -33,7 +33,6 @@ class FDBInput(GribInput):
     def __init__(
         self,
         context,
-        request_template: dict[str, Any],
         *,
         namer=None,
         fdb_config: dict | None = None,
@@ -46,8 +45,6 @@ class FDBInput(GribInput):
         ----------
         context : dict
             The context runner.
-        request_template : dict
-            The request template to use.
         namer : optional
             The namer to use for the input.
         fdb_config : dict, optional
@@ -55,22 +52,22 @@ class FDBInput(GribInput):
         fdb_userconfig : dict, optional
             The FDB userconfig to use.
         kwargs : dict, optional
-            Additional keyword arguments for creating the input state dictionary.
+            Additional keyword arguments for the request to FDB.
         """
         super().__init__(context, namer=namer)
-        self.request_template = dict(request_template)
         self.kwargs = kwargs
         self.configs = {"config": fdb_config, "userconfig": fdb_userconfig}
         self.fdb_config = fdb_config
         self.fdb_userconfig = fdb_userconfig
-        self.param_id_map = kwargs.pop("param_id_map", None)  # TODO: this is a temporary workaround for #191
+        # NOTE: this is a temporary workaround for #191 thus not documented
+        self.param_id_map = kwargs.pop("param_id_map", None)
         self.variables = self.checkpoint.variables_from_input(include_forcings=False)
 
     def create_input_state(self, *, date: Optional[Date]) -> State:
         date = np.datetime64(date).astype(datetime.datetime)
         dates = [date + h for h in self.checkpoint.lagged]
         ds = self.retrieve(variables=self.variables, dates=dates)
-        res = self._create_input_state(ds, variables=None, date=date, **self.kwargs)
+        res = self._create_input_state(ds, variables=None, date=date)
         return res
 
     def load_forcings_state(self, *, variables: List[str], dates: List[Date], current_state: State) -> State:
@@ -84,8 +81,8 @@ class FDBInput(GribInput):
             use_grib_paramid=self.context.use_grib_paramid,
             patch_request=self.context.patch_data_request,
         )
-        requests = [self.request_template | r for r in requests]
-        # TODO: this is a temporary workaround for #191
+        requests = [self.kwargs | r for r in requests]
+        # NOTE: this is a temporary workaround for #191
         for request in requests:
             request["param"] = [self.param_id_map.get(p, p) for p in request["param"]]
         sources = [ekd.from_source("fdb", request, stream=False, **self.configs) for request in requests]
