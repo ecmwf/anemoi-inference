4e78971e8affcb345c2ec7cd880e2f4d0d5d673a
b8raoult <53792887+b8raoult@users.noreply.github.com>
2025-05-06 11:34:14 +0100

    fix: support for netcdf missing values (#214)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dieter Van den Bleeken <dieter.vandenbleeken@meteo.be>

--- src/anemoi/inference/outputs/netcdf.py
+++ src/anemoi/inference/outputs/netcdf.py
@@ -31,19 +31,7 @@ LOCK = threading.RLock()
 @output_registry.register("netcdf")
 @main_argument("path")
 class NetCDFOutput(Output):
-    """NetCDF output class.
-
-    Parameters
-    ----------
-    context : dict
-        The context dictionary.
-    path : str
-        The path to save the NetCDF file.
-    output_frequency : int, optional
-        The frequency of output, by default None.
-    write_initial_state : bool, optional
-        Whether to write the initial state, by default None.
-    """
+    """NetCDF output class."""
 
     def __init__(
         self,
@@ -51,14 +39,39 @@ class NetCDFOutput(Output):
         path: str,
         output_frequency: Optional[int] = None,
         write_initial_state: Optional[bool] = None,
+        float_size: str = "f4",
+        missing_value: Optional[float] = np.nan,
     ) -> None:
+        """Initialize the NetCDF output object.
+
+        Parameters
+        ----------
+        context : dict
+            The context dictionary.
+        path : str
+            The path to save the NetCDF file.
+        output_frequency : int, optional
+            The frequency of output, by default None.
+        write_initial_state : bool, optional
+            Whether to write the initial state, by default None.
+        float_size : str, optional
+            The size of the float, by default "f4".
+        missing_value : float, optional
+            The missing value, by default np.nan.
+        """
+
         super().__init__(context, output_frequency=output_frequency, write_initial_state=write_initial_state)
 
         from netCDF4 import Dataset
 
         self.path = path
         self.ncfile: Optional[Dataset] = None
-        self.float_size = "f4"
+        self.float_size = float_size
+        self.missing_value = missing_value
+        if self.write_step_zero:
+            self.extra_time = 1
+        else:
+            self.extra_time = 0
 
     def __repr__(self) -> str:
         """Return a string representation of the NetCDFOutput object."""
@@ -95,6 +108,8 @@ class NetCDFOutput(Output):
             lead_time := getattr(self.context, "lead_time", None)
         ):
             time = lead_time // time_step
+            time += self.extra_time
+
         if reference_date := getattr(self.context, "reference_date", None):
             self.reference_date = reference_date
 
@@ -123,7 +138,6 @@ class NetCDFOutput(Output):
         self.longitude_var[:] = longitudes
 
         self.vars = {}
-        self.ensure_variables(state)
 
         self.n = 0
 
@@ -135,6 +149,7 @@ class NetCDFOutput(Output):
         state : State
             The state dictionary.
         """
+
         values = len(state["latitudes"])
 
         compression = {}  # dict(zlib=False, complevel=0)
@@ -148,13 +163,19 @@ class NetCDFOutput(Output):
                 chunksizes = tuple(int(np.ceil(x / 2)) for x in chunksizes)
 
             with LOCK:
+                missing_value = self.missing_value
+
                 self.vars[name] = self.ncfile.createVariable(
                     name,
                     self.float_size,
                     ("time", "values"),
                     chunksizes=chunksizes,
+                    fill_value=missing_value,
                     **compression,
                 )
+
+                self.vars[name].fill_value = missing_value
+                self.vars[name].missing_value = missing_value
 
     def write_step(self, state: State) -> None:
         """Write the state.
@@ -172,6 +193,7 @@ class NetCDFOutput(Output):
 
         for name, value in state["fields"].items():
             with LOCK:
+                LOG.info(f"🚧🚧🚧🚧🚧🚧 XXXXXX {name}, {self.n}, {value.shape}")
                 self.vars[name][self.n] = value
 
         self.n += 1
