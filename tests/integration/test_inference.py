e64a9c613cdb82bc0075305c1607c779a50e18d1
Gert Mertes <gert.mertes@ecmwf.int>
2025-06-20 13:22:54 +0000

    Restructure and parameterise integration test

--- tests/integration/test_inference.py
+++ tests/integration/test_inference.py
@@ -19,38 +19,39 @@ from anemoi.inference.runners import create_runner
 from anemoi.inference.testing import save_fake_checkpoint
 
 logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+INTEGRATION_ROOT = Path(__file__).resolve().parent
+
+# each model has its own folder in the integration tests directory
+# and contains at least a metadata.json and config.yaml file
+MODELS = [
+    path.name
+    for path in INTEGRATION_ROOT.iterdir()
+    if path.is_dir() and (path / "metadata.json").exists() and (path / "config.yaml").exists()
+]
 
 
-def create_checkpoint(tmp_dir):
-    """Fixture to create a fake checkpoint for testing."""
-    repo_root = Path(__file__).resolve().parent
-    metadata_path = repo_root / "checkpoints" / "single-o48-1.1.json"
-    checkpoint_path = tmp_dir / Path("checkpoint.pth")
+def create_config_and_checkpoint(tmp_dir: Path, model: str) -> Path:
+    """Create a fake configuration file and a checkpoint for testing."""
 
+    metadata_path = INTEGRATION_ROOT / model / "metadata.json"
+    config_path = INTEGRATION_ROOT / model / "config.yaml"
+    resolved_config_path = tmp_dir / "integration_test.yaml"
+
+    checkpoint_path = tmp_dir / Path("checkpoint.ckpt")
     save_fake_checkpoint(metadata_path, checkpoint_path)
-    return checkpoint_path
 
+    with open(config_path, "r") as f:
+        config = f.read()
 
-def create_config_and_checkpoint(tmp_dir):
-    """Fixture to create a fake configuration file and a checkpoint for testing."""
+    config = config.format(CHECKPOINT=checkpoint_path, TMP_DIR=tmp_dir)
+    LOG.info(f"Resolved config:\n{config}\n")
 
-    checkpoint_path = create_checkpoint(tmp_dir)
-    config_path = tmp_dir / "integration_test.yaml"
+    with open(resolved_config_path, "w") as f:
+        f.write(config)
 
-    with open(config_path, "w") as f:
-        f.write(
-            f"""
-        lead_time: 48h
-        post_processors:
-            - accumulate_from_start_of_forecast
-        checkpoint: {checkpoint_path}
-        input:
-            grib:  {tmp_dir}/input-single-o48-1.1.grib
-        output:
-            grib: {tmp_dir}/output.grib
-        """
-        )
-    return config_path
+    return resolved_config_path
 
 
 class Setup(NamedTuple):
@@ -58,20 +59,23 @@ class Setup(NamedTuple):
     tmp_dir: Path
 
 
-@pytest.fixture
-def test_setup() -> Setup:
-    url_dataset = "anemoi-integration-tests/inference/input-single-o48-1.1.grib"
-    grib_path = get_test_data(url_dataset)
+@pytest.fixture(params=MODELS)
+def test_setup(request) -> Setup:
+    model = request.param
+    grib_path = get_test_data(f"anemoi-integration-tests/inference/{model}/input.grib")
     tmp_dir = Path(grib_path).parent
-    config_path = create_config_and_checkpoint(tmp_dir)
+    config_path = create_config_and_checkpoint(tmp_dir, model)
     return Setup(config_path=config_path, tmp_dir=tmp_dir)
 
 
 def test_inference_on_checkpoint(test_setup: Setup) -> None:
     """Test the inference process using a fake checkpoint."""
+    overrides = {"lead_time": "48h", "device": "cpu", "trace_path": "trace.log"}
+    LOG.info(f"Config overrides: {overrides}")
+
     config = RunConfiguration.load(
         test_setup.config_path,
-        overrides=dict(device="cpu", trace_path="trace.log"),
+        overrides=overrides,
     )
     runner = create_runner(config)
     runner.execute()
