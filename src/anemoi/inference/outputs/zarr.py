9f70fa6520c6777b4a9a856db7bbd849473c56e1
Harrison Cook <harrison.cook@ecmwf.int>
2025-07-09 16:28:21 +0000

    from __future__ import annotations

--- src/anemoi/inference/outputs/zarr.py
+++ src/anemoi/inference/outputs/zarr.py
@@ -7,6 +7,8 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
+from __future__ import annotations
+
 import datetime
 import logging
 import os
