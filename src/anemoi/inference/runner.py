d69a5c2a8954a98d3cb304936f1c07efc8cd3e78
Gert Mertes <hey@gertmertes.be>
2025-07-24 08:50:53 +0100

    trace

--- src/anemoi/inference/runner.py
+++ src/anemoi/inference/runner.py
@@ -875,6 +875,8 @@ class Runner(Context):
 
             for n in source.variables_mask:
                 self._input_kinds[self._input_tensor_by_name[n]] = Kind(boundary=True, forcing=True, **source.kinds)
+                if self.trace:
+                    self.trace.from_source(self._input_tensor_by_name[n], source, "boundary forcings")
 
         # TO DO: add some consistency checks as above
         return input_tensor_torch
