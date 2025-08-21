d289affd5d10491690de3b6a17fd294543edfae9
Harrison Cook <Harrison.cook@ecmwf.int>
2025-08-21 11:43:01 +0200

    fix: device type in autocast (#317)
    ## Description
    Bug appears when running parallel
    
    ## What problem does this change solve?
    <!-- Describe if it's a bugfix, new feature, doc update, or breaking
    change -->
    
    ## What issue or task does this change relate to?
    <!-- link to Issue Number -->
    
    ##  Additional notes ##
    <!-- Include any additional information, caveats, or considerations that
    the reviewer should be aware of. -->
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)

--- src/anemoi/inference/runner.py
+++ src/anemoi/inference/runner.py
@@ -639,7 +639,7 @@ class Runner(Context):
 
             # Predict next state of atmosphere
             with (
-                torch.autocast(device_type=str(self.device), dtype=self.autocast),
+                torch.autocast(device_type=self.device.type, dtype=self.autocast),
                 ProfilingLabel("Predict step", self.use_profiler),
                 Timer(title),
             ):
