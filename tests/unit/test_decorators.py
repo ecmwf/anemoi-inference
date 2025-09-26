0dbbd5cb7a54608d4f3ccd4ef67bc197af98007b
Gert Mertes <hey@gertmertes.be>
2025-09-26 11:23:57 +0100

    add tests

---
+++ tests/unit/test_decorators.py
@@ -0,0 +1,29 @@
+from anemoi.inference.decorators import main_argument
+
+
+def test_main_argument():
+    @main_argument("path")
+    def _fn(context, flag=True, path=None):
+        return context, flag, path
+
+    context, flag, path = _fn("context", "path")
+
+    assert context == "context"
+    assert flag is True
+    assert path == "path"
+
+    @main_argument("path")
+    class _Cls:
+        def __init__(self, context, flag=True, path=None):
+            self.context = context
+            self.flag = flag
+            self.path = path
+
+    cls = _Cls("context", "path")
+
+    assert cls.context == "context"
+    assert cls.flag is True
+    assert cls.path == "path"
+
+    assert isinstance(main_argument("path")(_fn), type(_fn))
+    assert isinstance(main_argument("path")(_Cls), type)
