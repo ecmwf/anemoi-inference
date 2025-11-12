aba8e85df3d08263f8cd214e2d796c98f7869e05
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-11-12 11:23:52 +0100

    Fix printing

--- src/anemoi/inference/runner.py
+++ src/anemoi/inference/runner.py
@@ -889,9 +889,16 @@ class Runner(Context):
         kinds : dict
             The kinds.
         """
-        assert tensor_numpy.ndim == 4, tensor_numpy.shape
+
+        # (multi_step_input, members, variables, values)
+        if tensor_numpy.ndim == 4:
+            var_idx = 2
+        else:
+            assert tensor_numpy.ndim == 3, tensor_numpy.shape
+            var_idx = 1
+
         assert tensor_numpy.shape[0] in (1, self.checkpoint.multi_step_input), tensor_numpy.shape
-        assert tensor_numpy.shape[2] == len(tensor_by_name), tensor_numpy.shape
+        assert tensor_numpy.shape[var_idx] == len(tensor_by_name), tensor_numpy.shape
 
         t = []
         for k, v in enumerate(tensor_by_name):
@@ -961,10 +968,11 @@ class Runner(Context):
 
         # output_tensor_numpy = output_tensor_numpy.cpu().numpy()
 
+        # (members, values, variables)
         if len(output_tensor_numpy.shape) == 3:
             output_tensor_numpy = output_tensor_numpy[np.newaxis, ...]  # Add multi_step_input
 
-        output_tensor_numpy = np.swapaxes(output_tensor_numpy, -2, -1)  # (multi_step_input, variables, values)
+        output_tensor_numpy = np.swapaxes(output_tensor_numpy, -2, -1)  # (multi_step_input, members, variables, values)
 
         self._print_tensor(title, output_tensor_numpy, self._output_tensor_by_name, self._output_kinds)
 
