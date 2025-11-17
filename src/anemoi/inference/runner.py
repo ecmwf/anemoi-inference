d6572b6d7210c202d2971366ddaaa03ed71d5805
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-11-17 16:09:55 +0100

    Remove wrong output cpu copies

--- src/anemoi/inference/runner.py
+++ src/anemoi/inference/runner.py
@@ -784,13 +784,13 @@ class Runner(Context):
                         new_state["fields"][self.checkpoint.output_tensor_index_to_variable[i]] = output[..., i]
 
                 if (s == 0 and self.verbosity > 0) or self.verbosity > 1:
-                    self._print_output_tensor("Output tensor", output.cpu().numpy())
+                    self._print_output_tensor("Output tensor", output)
 
                 if self.trace:
                     self.trace.write_output_tensor(
                         date,
                         s,
-                        output.cpu().numpy(),
+                        output,
                         self.checkpoint.output_tensor_index_to_variable,
                         self.checkpoint.timestep,
                     )
