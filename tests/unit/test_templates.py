8719fed19f533345183477bac83b705ad3fc4d97
Gert Mertes <gert.mertes@ecmwf.int>
2025-11-18 16:10:40 +0000

    refactor tests

--- tests/unit/test_templates.py
+++ tests/unit/test_templates.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pytest
 import yaml
 from anemoi.utils.testing import GetTestData
 from earthkit.data.readers.grib.codes import GribField
@@ -8,112 +9,85 @@ from pytest_mock import MockerFixture
 from anemoi.inference.checkpoint import Checkpoint
 from anemoi.inference.grib.templates.manager import TemplateManager
 from anemoi.inference.testing import fake_checkpoints
+from anemoi.inference.testing import files_for_tests
 
 
-@fake_checkpoints
-def test_manager_builtin(mocker: MockerFixture):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
+@pytest.fixture
+def manager(mocker: MockerFixture):
+    c = Checkpoint(files_for_tests("unit/checkpoints/simple.yaml"))
     owner = mocker.MagicMock()
     owner.context.checkpoint = c
 
-    manager = TemplateManager(owner)
+    @fake_checkpoints
+    def _manager(config=None):
+        return TemplateManager(owner, templates=config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    return _manager
+
+
+def test_manager_builtin(manager):
+    manager = manager()
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "lsm"  # lsm is used as the builtin template for surface fields
 
 
-@fake_checkpoints
-def test_manager_file(mocker: MockerFixture, get_test_data: GetTestData):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_file(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
-
     config = {"file": template_file}
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "10u"  # first field in the file
 
 
-@fake_checkpoints
-def test_manager_file_last(mocker: MockerFixture, get_test_data: GetTestData):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_file_last(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
-
     config = {"file": {"path": template_file, "mode": "last"}}
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "v"  # last field in the file
 
 
-@fake_checkpoints
-def test_manager_file_skip_variable(mocker: MockerFixture, get_test_data: GetTestData):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_file_skip_variable(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
-
     config = {"file": {"path": template_file, "variable": "10u"}}
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert template is None
 
 
-@fake_checkpoints
-def test_manager_file_auto(mocker: MockerFixture, get_test_data: GetTestData):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_file_auto(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
-
     config = {"file": {"path": template_file, "mode": "auto"}}
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "2t"
 
 
-@fake_checkpoints
-def test_manager_file_auto_unknown(mocker: MockerFixture, get_test_data: GetTestData):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_file_auto_unknown(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
-
     config = {"file": {"path": template_file, "mode": "auto"}}
 
-    manager = TemplateManager(owner, templates=config)
-    typed = c.typed_variables
+    manager = manager(config)
+    typed = manager.typed_variables
     typed["unknown"] = typed["2t"]
 
     template = manager.template("unknown", state={}, typed_variables=typed)
     assert template is None
 
 
-@fake_checkpoints
-def test_manager_samples(mocker: MockerFixture, get_test_data: GetTestData, tmp_path: Path):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_samples(manager, get_test_data: GetTestData, tmp_path: Path):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
     index = [
         [{"grid": "O96", "levtype": "pl"}, template_file],
@@ -127,19 +101,14 @@ def test_manager_samples(mocker: MockerFixture, get_test_data: GetTestData, tmp_
         "samples": {"index_path": str(index_file)},
     }
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "10u"  # first field in the file
 
 
-@fake_checkpoints
-def test_manager_samples_index_str(mocker: MockerFixture, get_test_data: GetTestData, tmp_path: Path):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_samples_index_str(manager, get_test_data: GetTestData, tmp_path: Path):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
     index = [
         [{"grid": "O96", "levtype": "pl"}, template_file],
@@ -153,19 +122,14 @@ def test_manager_samples_index_str(mocker: MockerFixture, get_test_data: GetTest
         "samples": str(index_file),
     }
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "10u"  # first field in the file
 
 
-@fake_checkpoints
-def test_manager_samples_direct_index(mocker: MockerFixture, get_test_data: GetTestData, tmp_path: Path):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
+def test_manager_samples_direct_index(manager, get_test_data: GetTestData):
     template_file = get_test_data("anemoi-integration-tests/inference/single-o48-1.1/input.grib")
     index = [
         [{"grid": "O96", "levtype": "pl"}, template_file],
@@ -174,25 +138,20 @@ def test_manager_samples_direct_index(mocker: MockerFixture, get_test_data: GetT
 
     config = {"samples": index}
 
-    manager = TemplateManager(owner, templates=config)
+    manager = manager(config)
 
-    template = manager.template("2t", state={}, typed_variables=c.typed_variables)
+    template = manager.template("2t", state={}, typed_variables=manager.typed_variables)
     assert isinstance(template, GribField)
     assert template.metadata("param") == "10u"  # first field in the file
 
 
-@fake_checkpoints
-def test_manager_input(mocker: MockerFixture):
-    c = Checkpoint("unit/checkpoints/simple.ckpt")
-    owner = mocker.MagicMock()
-    owner.context.checkpoint = c
-
-    manager = TemplateManager(owner)
+def test_manager_input(manager):
+    manager = manager()
     state = {
         "_grib_templates_for_output": {
             "2t": "2t_input_template",
         }
     }
 
-    template = manager.template("2t", state=state, typed_variables=c.typed_variables)
+    template = manager.template("2t", state=state, typed_variables=manager.typed_variables)
     assert template == "2t_input_template"
