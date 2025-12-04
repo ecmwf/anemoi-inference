06edc8df10eac06a9d4b38d04dbfcbb880a4b614
Gert Mertes <gert.mertes@ecmwf.int>
2025-12-04 13:31:43 +0000

    test: add time processing test

--- tests/unit/test_grib.py
+++ tests/unit/test_grib.py
@@ -1,7 +1,10 @@
 import sys
 
 import pytest
+from anemoi.transform.variables import Variable
+from anemoi.utils.dates import as_timedelta
 
+from anemoi.inference.grib.encoding import encode_time_processing
 from anemoi.inference.grib.encoding import render_template
 
 
@@ -31,3 +34,63 @@ def test_render_template(template, handle, expected):
     """
     result = render_template(template, handle)
     assert result == expected
+
+
+tp = Variable.from_dict(
+    "tp",
+    {
+        "mars": {
+            "param": "tp",
+            "levtype": "sfc",
+        },
+        "process": "accumulation",
+        "period": [0, 6],
+    },
+)
+z = Variable.from_dict(
+    "z",
+    {
+        "mars": {
+            "param": "z",
+            "levtype": "sfc",
+        }
+    },
+)
+
+
+@pytest.mark.parametrize(
+    "variable, date, time, step, expected_keys",
+    [
+        (
+            tp,
+            20250101,
+            0,
+            as_timedelta(0),
+            {"date": 20241231, "time": 1800, "startStep": 0, "endStep": 6, "stepType": "accum"},
+        ),
+        (
+            tp,
+            20250101,
+            0,
+            as_timedelta(6),
+            {"date": 20250101, "time": 0, "startStep": 0, "endStep": 6, "stepType": "accum"},
+        ),
+        (z, 20250101, 0, as_timedelta(0), {"date": 20250101, "time": 0, "step": 0, "stepType": "instant"}),
+    ],
+)
+def test_encode_time_processing(variable, date, time, step, expected_keys):
+    encoding = {"date": date, "time": time}
+
+    encode_time_processing(
+        result=encoding,
+        template=None,
+        variable=variable,
+        step=step,
+        previous_step=None,
+        start_steps={},
+        edition=1,
+        ensemble=False,
+    )
+
+    for key, expected_value in expected_keys.items():
+        assert encoding[key] == expected_value
