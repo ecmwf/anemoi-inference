5731f0ea416b3352c01ed46b2f01da08b8d55105
Gert Mertes <13658335+gmertes@users.noreply.github.com>
2025-12-04 13:34:20 +0000

    feat(grib): Improve template manager (#383)
    ## Description
    Improve the grib template manager in a backwards compatible way.
    
    - [x] Improve documentation on how users can provide their own templates
    - [x] For the `samples` provider, add the option for the sample index to
    be specified directly in the inference config
    - [x] For the `file` provider, add an option for the file to contain
    many parameters, and do a lookup based on the param name.
    - [x] In the case of grib input, the template manager automatically
    injects templates found in the input grib at the highest priority.
    Refactor this so that this becomes its own standalone template provider
    called `input`, this gives more flexibility to enable/disable
    combinations of template providers and set the priority..
    - [x] `input` provider fallback mode: force an output variable to use
    one of the input variables
    - [x] Improve logging of the template manager. Right now it only logs in
    the case of errors. Also log in the case of success so it's easier to
    see which templates are being used from which provider.
    - [x] tests for everything, including backwards compatibility
    
    ***As a contributor to the Anemoi framework, please ensure that your
    changes include unit tests, updates to any affected dependencies and
    documentation, and have been tested in a parallel setting (i.e., with
    multiple GPUs). As a reviewer, you are also responsible for verifying
    these aspects and requesting changes if they are not adequately
    addressed. For guidelines about those please refer to
    https://anemoi.readthedocs.io/en/latest/***
    
    By opening this pull request, I affirm that all authors agree to the
    [Contributor License
    Agreement.](https://github.com/ecmwf/codex/blob/main/Legal/contributor_license_agreement.md)
    
    
    <!-- readthedocs-preview anemoi-inference start -->
    ----
    📚 Documentation preview 📚:
    https://anemoi-inference--383.org.readthedocs.build/en/383/
    
    <!-- readthedocs-preview anemoi-inference end -->

--- src/anemoi/inference/grib/templates/__init__.py
+++ src/anemoi/inference/grib/templates/__init__.py
@@ -9,6 +9,7 @@
 
 
 import logging
+from typing import TYPE_CHECKING
 from typing import Any
 
 import earthkit.data as ekd
@@ -16,6 +17,10 @@ import yaml
 from anemoi.utils.registry import Registry
 
 from anemoi.inference.config import Configuration
+from anemoi.inference.output import Output
+
+if TYPE_CHECKING:
+    from .manager import TemplateManager
 
 LOG = logging.getLogger(__name__)
 
@@ -23,12 +28,12 @@ LOG = logging.getLogger(__name__)
 template_provider_registry = Registry(__name__)
 
 
-def create_template_provider(owner: Any, config: Configuration) -> "TemplateProvider":
+def create_template_provider(owner: Output, config: Configuration) -> "TemplateProvider":
     """Create a template provider from the given configuration.
 
     Parameters
     ----------
-    owner : Any
+    owner : Output
         The owner of the template provider.
     config : Configuration
         The configuration for the template provider.
@@ -44,17 +49,20 @@ def create_template_provider(owner: Any, config: Configuration) -> "TemplateProv
 class TemplateProvider:
     """Base class for template providers."""
 
-    def __init__(self, manager: Any) -> None:
+    def __init__(self, manager: "TemplateManager") -> None:
         """Initialize the template provider.
 
         Parameters
         ----------
-        manager : Any
+        manager : TemplateManager
             The manager for the template provider.
         """
         self.manager = manager
 
-    def template(self, variable: str, lookup: dict[str, Any]) -> ekd.Field:
+    def __repr__(self):
+        return f"{self.__class__.__name__}"
+
+    def template(self, variable: str, lookup: dict[str, Any], **kwargs) -> ekd.Field | None:
         """Get the template for the given variable and lookup.
 
         Parameters
@@ -63,10 +71,12 @@ class TemplateProvider:
             The variable to get the template for.
         lookup : Dict[str, Any]
             The lookup dictionary.
+        kwargs
+            Extra arguments for specific template providers.
 
         Returns
         -------
-        ekd.Field
+        ekd.Field | None
             The template field.
         """
         raise NotImplementedError()
@@ -75,55 +85,45 @@ class TemplateProvider:
 class IndexTemplateProvider(TemplateProvider):
     """Template provider based on an index file."""
 
-    def __init__(self, manager: Any, index_path: str) -> None:
+    def __init__(self, manager: "TemplateManager", index: str | list) -> None:
         """Initialize the index template provider.
 
         Parameters
         ----------
-        manager : Any
+        manager : TemplateManager
             The manager for the template provider.
-        index_path : str
-            The path to the index file.
+        index : str | list
+            The path to the index.yaml file, or its contents directly as a list.
         """
         super().__init__(manager)
-        self.index_path = index_path
+        self.index_path = index
 
-        with open(index_path) as f:
-            self.templates = yaml.safe_load(f)
+        if isinstance(index, str):
+            with open(index) as f:
+                self.templates = yaml.safe_load(f)
+        else:
+            self.templates = index
 
         if not isinstance(self.templates, list):
-            raise ValueError("Invalid templates.yaml, must be a list")
+            raise ValueError(f"Invalid index, must be a list. Got {self.templates}")
 
         # TODO: use pydantic
         for template in self.templates:
             if not isinstance(template, list):
-                raise ValueError(f"Invalid template in templates.yaml, must be a list: {template}")
+                raise ValueError(f"Invalid template index element, must be a list. Got {template}")
             if len(template) != 2:
-                raise ValueError(f"Invalid template in templates.yaml, must have exactly 2 elements: {template}")
+                raise ValueError(
+                    f"Expected template index to be a 2-elements list as `[matching filter, grib file]`. Got {template}."
+                )
 
             match, grib = template
             if not isinstance(match, dict):
-                raise ValueError(f"Invalid match in templates.yaml, must be a dict: {match}")
+                raise ValueError(f"Invalid match in index element, must be a dict: {match}")
 
             if not isinstance(grib, str):
-                raise ValueError(f"Invalid grib in templates.yaml, must be a string: {grib}")
+                raise ValueError(f"Invalid grib in index element, must be a string: {grib}")
 
-    def template(self, variable: str, lookup: dict[str, Any]) -> ekd.Field | None:
-        """Get the template for the given variable and lookup.
-
-        Parameters
-        ----------
-        variable : str
-            The variable to get the template for.
-        lookup : Dict[str, Any]
-            The lookup dictionary.
-
-        Returns
-        -------
-        Optional[ekd.Field]
-            The template field if found, otherwise None.
-        """
-
+    def template(self, variable: str, lookup: dict[str, Any], **kwargs) -> ekd.Field | None:
         def _as_list(value: Any) -> list[Any]:
             if not isinstance(value, list):
                 return [value]
@@ -132,7 +132,7 @@ class IndexTemplateProvider(TemplateProvider):
         for template in self.templates:
             match, grib = template
             if LOG.isEnabledFor(logging.DEBUG):
-                LOG.debug("%s", [(lookup.get(k), _as_list(v)) for k, v in match.items()])
+                LOG.debug(f"Matching {match} -> {[(lookup.get(k), _as_list(v)) for k, v in match.items()]}")
 
             if all(lookup.get(k) in _as_list(v) for k, v in match.items()):
                 return self.load_template(grib, lookup)
